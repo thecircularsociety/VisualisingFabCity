1d855e57fc625484d07855f1017864db32f4f8f0
beth-holmes <beth.holmes123@gmail.com>
2021-04-07 11:19:30 +0100

    Tidied app

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,9 +1,28 @@
 import * as React from 'react';
-import { ForceGraph3D, ForceGraph2D } from 'react-force-graph';
-import SpriteText from 'three-spritetext';
+import { ForceGraph2D } from 'react-force-graph';
 import * as Immutable from 'immutable';
-import { Object3D, Sprite, TextGeometry } from 'three';
-import * as THREE from 'three';
+import Dropdown from 'react-dropdown';
+
+const emptyProperties: Immutable.Map<number, Node> = Immutable.Map();
+const emptyNetwork: Network = {
+    nodes: [],
+    links: []
+}
+
+const emptyNode: Node = {
+    id: -1,
+    name: "",
+    type: ""
+}
+
+const emptyGraphProps: NetworkGraphProps = {
+    dataSource: "FabCity.json",
+    getColourFromCategory: getColourFromFabCityCategory
+}
+type NetworkGraphProps = {
+    dataSource: string,
+    getColourFromCategory: (category: string) => string
+}
 
 type Node = {
     id: number,
@@ -21,8 +40,54 @@ type Network = {
     links: Link[]
 }
 
-function getColourFromType(type: string): string {
-    switch (type) {
+function NetworkGraph(props: NetworkGraphProps) {
+    var [network, setNetwork] = React.useState(emptyNetwork);
+    var [properties, setProperties] = React.useState(emptyProperties);
+    var [loaded, setLoaded] = React.useState(false);
+
+    React.useEffect(() => {
+        async function fetchNetworkData(): Promise<Network> {
+            return await fetch(props.dataSource).then((response) => {
+                return response.text().then(body => JSON.parse(body))
+            })
+        }
+
+        fetchNetworkData().then(networkData => {
+            setNetwork(networkData);
+            networkData.nodes.forEach(node => {
+                setProperties(currentProperties => {
+                    return currentProperties.set(node.id, node)
+                })
+            })
+            setLoaded(l => true);
+        })
+    }, [props.dataSource]);
+
+    if (loaded) {
+        return <ForceGraph2D graphData={network}
+            linkWidth={2}
+            backgroundColor="#FFFFFF"
+            nodeLabel={graphNode => {
+                const node = properties.get(graphNode.id as number, emptyNode);
+                return node.type;
+            }}
+            nodeCanvasObject={({ id, x, y }, ctx) => {
+                const node = properties.get(id as number, emptyNode);
+                ctx.fillStyle = props.getColourFromCategory(node.type);
+                ctx.font = '10px Sans-Serif';
+                ctx.textAlign = 'center';
+                ctx.textBaseline = 'middle';
+                ctx.fillText(node.name, x as number, y as number);
+            }}
+        />
+    }
+    else {
+        return <div></div>
+    }
+}
+
+function getColourFromFabCityCategory(category: string): string {
+    switch (category) {
         case "Education": return "#f72585"
         case "Business": return "#7209b7"
         case "Partnership": return "#3a0ca3"
@@ -31,76 +96,24 @@ function getColourFromType(type: string): string {
         default: return "black"
     }
 }
-
 function App() {
-    const emptyProperties: Immutable.Map<number, Node> = Immutable.Map();
-    const emptyNetwork: Network = {
-        nodes: [],
-        links: []
-    }
-    const emptyNode: Node =
-    {
-        id: -1,
-        name: "",
-        type: ""
-    }
+    var [graphProps, setGraphProps] = React.useState(emptyGraphProps);
 
-    function getColourFromId(id: number): string {
-        const type = properties.map(x => x.type).get(id, "");
-        return getColourFromType(type);
-    }
-
-    var [network, setNetwork] = React.useState(emptyNetwork);
-    var [properties, setProperties] = React.useState(emptyProperties);
-    var [loaded, setLoaded] = React.useState(false);
-
-    React.useEffect(() => {
-        async function fetchNetworkData(): Promise<Network> {
-            return await fetch("data.json").then((response) => {
-
-                var result = response.text().then(body => JSON.parse(body))
-                //console.log(result);
-                return result;
-            }
-
-            )
-        }
-
-        fetchNetworkData().then(networkData => {
-            setNetwork(networkData);
-            networkData.nodes.map(node => {
-                setProperties(currentProperties => {
-
-                    return currentProperties.set(node.id, node)
-                })
-            })
-            setLoaded(l => true);
-        })
-    }, []);
-
-    return (!loaded) ? <div></div> : <ForceGraph2D graphData={network}
-        linkWidth={2}
-        backgroundColor="#e7ecef"
-        nodeLabel={graphNode => {
-            const node = properties.get(graphNode.id as number, emptyNode);
-            return node.type;
-        }}
-        nodeCanvasObject={({ id, x, y }, ctx) => {
-            const node = properties.get(id as number, emptyNode);
-            ctx.fillStyle = getColourFromType(node.type);
-            ctx.font = '10px Sans-Serif'; ctx.textAlign = 'center'; ctx.textBaseline = 'middle'; ctx.fillText(node.name, x, y);
-        }}
-
-        //nodeThreeObject={graphNode => {
-        //    const node = properties.get(graphNode.id as number, emptyNode);
-        //    const colour = getColourFromType(node.type);
-        //    const sprite = new SpriteText(node.name, 10, colour);
-        //    sprite.fontFace = "Arial"
-        //    sprite.fontWeight = "bold"
-        //    return sprite;
-        //}} 
-
-    />;
+    return (
+        <div>
+            <Dropdown
+                options={options}
+                onChange={x => {
+                    setGraphProps(getPropsFromName(x.value))
+                }}
+                value={options[0]}
+            />
+            <NetworkGraph
+                dataSource={graphProps.dataSource}
+                getColourFromCategory={graphProps.getColourFromCategory}
+            />
+        </div>
+    )
 }
 
 export default App;
